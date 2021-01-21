0fba763c0f856c56ce8efa8a8479e96a7ed6c4c0
dude lao <brycelipscomb@gmail.com>
2021-01-20 19:49:32 -0500

    completed mvp

---
+++ api/cars/cars-router.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const db = require('../../data/db-config');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    db('cars')
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(next);
+});
+
+router.get('/:id', (req, res, next) => {
+    const { id } = req.params;
+
+    db('cars').where({ id }).first()
+        .then(car => {
+            res.status(200).json(car);
+        })
+        .catch(next);
+})
+
+router.post('/', (req, res, next) => {
+    const carData = req.body;
+    db('cars').insert(carData)
+        .then(ids => {
+            return db('cars').where({ id: ids[0] })
+        })
+        .then(newCarEntry => {
+            res.status(201).json(newCarEntry)
+        })
+        .catch(next);
+});
+
+router.use((err, req, res, next) => {
+    const env = (process.env.NODE_ENV || 'development');
+    const message = env === 'development'
+      ? err.message
+      : 'something bad happened';
+    res.status(500).json(message);
+  });
+
+module.exports = router;
